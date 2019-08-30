eaa5044ae6d5bdf3988595bf20442166426c80a0
Simon Li <orpheus+devel@gmail.com>
2019-08-30 21:21:31 +0100

    Default pkcs12 algorithm is sufficient

--- icessl/certs.sh
+++ icessl/certs.sh
@@ -14,7 +14,7 @@ fi
 openssl req -new -nodes -x509 -subj "$OWNER/CN=$CN" -days $DAYS -keyout server.key -out server.pem -extensions v3_ca
 echo Created server.key server.pem
 
-openssl pkcs12 -keypbe PBE-SHA1-3DES -certpbe PBE-SHA1-3DES -export -out server.p12 -inkey server.key -in server.pem -name server -password pass:"$PASSWORD"
+openssl pkcs12 -export -out server.p12 -inkey server.key -in server.pem -name server -password pass:"$PASSWORD"
 echo Created server.p12 
 
 # To view pkcs12 file:
